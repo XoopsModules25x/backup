d711adf67bfae91ba58987c53cff8fa265655731
mambax7 <mambax7@gmail.com>
2017-03-29 04:27:15 -0400

    update for PayPal

--- admin/about.php
+++ admin/about.php
@@ -8,6 +8,6 @@ $module_info =& $module_handler->get($xoopsModule->getVar("mid"));
 $aboutAdmin = new ModuleAdmin();
 
 echo $aboutAdmin->addNavigation('about.php');
-echo $aboutAdmin->renderabout('6KJ7RW5DR3VTJ', false);
+echo $aboutAdmin->renderabout('xoopsfoundation@gmail.com', false);
 
 include 'admin_footer.php';
